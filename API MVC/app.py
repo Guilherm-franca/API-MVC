89d0cafdacf0e4374121a3bad3ab5f4708d4d08a
Guilherme Alexandre <guilherme.alexandre@aluno.faculdadeimpacta.com.br>
2025-09-15 09:43:57 -0300

    Conteinerização

--- API MVC/app.py
+++ API MVC/app.py
@@ -59,4 +59,5 @@ api.add_namespace(task_ns, path='/tasks')
 api.add_namespace(user_ns, path='/usuarios')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
